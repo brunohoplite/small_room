a07b07bbdb072429f6749875a6abd06888ab1288
Bruno Malli <bruno.malli@ossiaco.com>
2021-10-25 19:39:54 -0400

    Initial commit

---
+++ led_strip.py
@@ -0,0 +1,14 @@
+import RPi.GPIO
+
+class LedStrip:
+    def __init__(self, pin):
+        self.pin = pin
+
+    def ledOn(self):
+        pass
+
+    def ledOff(self):
+        pass
+
+    def ledPwm(self, dt):
+        pass
